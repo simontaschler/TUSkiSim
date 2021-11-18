f0f7372e5abb6169029aced486f1e1f4c129fc07
simontaschler <simon.taschler@infominds.eu>
2021-11-18 11:47:10 +0100

    implemented Hut and Track

--- TUSkiSim.Lib/Hut.cs
+++ TUSkiSim.Lib/Hut.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace TUSkiSim.Lib
 {
-    class Hut
+    public class Hut
     {
+        private int avgStay;
+        private int guests;
+        private int maxGuests;
+        private string name;
+
+        //Properties ersetzen Get-Methoden
+        public int AverageStay => avgStay;
+        public int Guests => guests;
+        public int MaxGuests => maxGuests;
+
+        public Hut(string name, int maxGuests, int avgStay) 
+        {
+            this.name = name;
+            this.maxGuests = maxGuests;
+            this.avgStay = avgStay;
+            guests = 0;
+        }
+
+        public void AddGuests(int numGuests = 1) =>
+            guests += numGuests;
     }
 }
--- TUSkiSim.Lib/Track.cs
+++ TUSkiSim.Lib/Track.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace TUSkiSim.Lib
 {
-    class Track
+    public class Track
     {
+        private readonly int capacity;
+        private readonly Hut hut;
+        private readonly int length;
+        private readonly int level;  //1,2,3 => durch enum ersetzen
+        private readonly Lift lift;
+        private readonly int number;
+        private int peopleOnTrack;
+        //private readonly double workload;
+
+        public Hut Hut => hut;
+        public int Length => length;
+        public int Level => level;
+        public Lift Lift => lift;
+        public int Number => number;
+        public int PeopleOnTrack => peopleOnTrack;
+
+        public Track(int number, int length, int level, int capacity, Lift lift, Hut hut = null) 
+        {
+            this.number = number;
+            this.length = length;
+            this.level = level;
+            this.capacity = capacity;
+            this.lift = lift;
+            this.hut = hut;
+        }
+
+        public double CalcWorkload() =>
+            ((double)peopleOnTrack) / capacity;
+
+        public void ChangePeopleOnTrack(int numPeople) =>
+            peopleOnTrack = numPeople;
     }
 }
