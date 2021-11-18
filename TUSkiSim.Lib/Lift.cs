23f75b368db49825dc3e265d6defbb240d300ca1
simontaschler <simon.taschler@infominds.eu>
2021-11-18 10:55:04 +0100

    implemented Lift

--- TUSkiSim.Lib/Lift.cs
+++ TUSkiSim.Lib/Lift.cs
@@ -8,6 +8,33 @@ namespace TUSkiSim.Lib
 {
     public abstract class Lift
     {
+        protected int number;
+        protected int elements;
+        protected int length;
+        protected int velocity;
+        private int waitingQueue;
 
+        //Properties ersetzen Get-Methoden
+        public int Number => number;
+        public int TravelTime => length / velocity;
+        public int WaitingQueue => waitingQueue;
+
+        protected Lift(int number, int velocity, int length, int elements) 
+        {
+            this.number = number;
+            this.velocity = velocity;
+            this.length = length;
+            this.elements = elements;
+        }
+
+        public abstract int CalcFlowRate();
+
+        public void AddQueue() => 
+            waitingQueue++;
+
+        public void RedWaitingQueue() =>
+            waitingQueue -= waitingQueue - CalcFlowRate() <= 0
+                ? waitingQueue
+                : CalcFlowRate();
     }
 }
